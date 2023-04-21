62e5e79e06ee2f95f838eca632d3f40705d97da9
roseyangina <93233603+roseyangina@users.noreply.github.com>
2023-04-21 15:02:58 -0400

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -8,7 +8,7 @@
 void merge(std::vector<std::pair<std::string, double>>& arr, int left, int mid, int right);
 void mergeSort(std::vector<std::pair<std::string, double>>& arr, int left, int right);
 //city
-void printSortedCityDelayLikelihoods(const std::unordered_map<std::string, double>& cityDelayLikelihoods);
+void sortedCityDelay(std::unordered_map<std::string, double>& cityDelayLikelihoods);
 //airline
-void printSortedAirlineDelayLikelihoods(const std::unordered_map<std::string, double>& airlineDelayLikelihoods);
+void sortedAirlineDelay(std::unordered_map<std::string, double>& airlineDelayLikelihoods);
 
