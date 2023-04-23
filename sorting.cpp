ce1bed1f9d835905a3035f426daa8af323dcf736
SofiaBLynch <113479969+SofiaBLynch@users.noreply.github.com>
2023-04-23 00:50:43 -0400

    Update sorting.cpp
    Added in the quick sort algorithms

--- sorting.cpp
+++ sorting.cpp
@@ -111,3 +111,109 @@ void sortedAirlineDelay(std::unordered_map<std::string, double>& airlineDelayCal
 }
 
 
+//Quick uses information from slide 122 of the Graphs ppt
+void quickSort(std::vector<std::pair<std::string, double>>&arr, int low, int high)
+{
+    if (low<high)
+    {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
+    }
+}
+
+int partition(std::vector<std::pair<std::string, double>>&arr, int low, int high)
+{
+    int pivot = arr[low].second;
+    int up = low, down = high;
+    while (up < down)
+    {
+        for (int j = up; j < high; j++)
+        {
+            if (arr[up].second > pivot)
+            {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--)
+        {
+            if (arr[down].second < pivot)
+            {
+                break;
+            }
+            down--;
+        }
+        if (up < down)
+        {
+            std::pair<std::string, double> temp1(arr[up].first, arr[up].second);
+            arr[up].first = arr[down].first;
+            arr[up].second = arr[down].second;
+            arr[down].first = temp1.first;
+            arr[down].second = temp1.second;
+        }
+    }
+        std::pair<std::string, double> temp2(arr[low].first, arr[low].second);
+        arr[low].first = arr[down].first;
+        arr[low].second = arr[down].second;
+        arr[down].first = temp2.first;
+        arr[down].second = temp2.second;
+        return down;
+}
+
+//City Quick Sort
+void sortedCityDelayQ(std::unordered_map<std::string, double>& cityDelayCalc) {
+    std::vector<std::pair<std::string, double>> cityDelayVec; //city and delay vector
+
+    cityDelayVec.reserve(cityDelayCalc.size()); //for vector capacity https://cplusplus.com/reference/vector/vector/reserve/
+    for (auto& elements : cityDelayCalc) { //insert elements from map to vector for sorting https://www.geeksforgeeks.org/sorting-a-map-by-value-in-c-stl/amp/
+        cityDelayVec.push_back(elements);
+    }
+
+    quickSort(cityDelayVec, 0, cityDelayVec.size() - 1); //sort from first to last element
+
+    for (auto& elements : cityDelayVec) { // get city and the delay
+
+        std::cout << "City: " << elements.first << ", Delay Likelihood: " << elements.second << "%" << std::endl;
+        std::cout << std::endl;
+
+    }
+}
+
+//Airline Quick Sort
+void sortedAirlineDelayQ(std::unordered_map<std::string, double>& airlineDelayCalc) {
+
+    std::vector<std::pair<std::string, double>> airlineDelayVec;
+
+    airlineDelayVec.reserve(airlineDelayCalc.size()); //since it is a large data set
+
+    for (auto& elements : airlineDelayCalc) { //insert elements from map to vector for sorting
+        airlineDelayVec.push_back(elements);
+    }
+
+    quickSort(airlineDelayVec, 0, airlineDelayVec.size() - 1); //sorting from the first element to the last element
+
+    std::cout << "Airline Delay Likelihoods:" << std::endl;
+
+    for (auto& pair : airlineDelayVec) {
+        std::string& airlineCode = pair.first;
+        double delayLikelihood = pair.second;
+
+        std::cout << airlineCode << ": " << delayLikelihood << "%" << std::endl;
+        std::cout << std::endl;
+
+    }
+    // Print top 5 worst airlines
+    std::cout << "Top 5 Worst Airlines to Fly to Based on Delay Likelihood: " << std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cout << (i+1) << ". " << airlineDelayVec.at(i).first << std::endl;
+    }
+    // Print top 5 best airlines
+    std::cout << "Top 5 Best Airlines to Fly to Based on Delay Likelihood: " << std::endl;
+    int size = airlineDelayVec.size();
+    for(int i = 0; i < 5; i++){
+        std::cout << (i+1) << ". " << airlineDelayVec.at(size - 1 - i).first << std::endl;
+    }
+    std::cout << std::endl;
+}
+
